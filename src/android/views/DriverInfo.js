1d52dfdacfa1a28b4dd7118d2f24522937cf3450
rb yu <rbyu@rbdeMacBook-Air.local>
2017-09-12 09:07:07 +0800

    ok

--- src/android/views/DriverInfo.js
+++ src/android/views/DriverInfo.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import {
     Text,
     View,
-    InteractionManager
+    InteractionManager,
+    ActivityIndicator
 } from 'react-native'
 import { Button, Icon } from 'native-base'
 import RecordListItem from '../components/RecordListItem'
@@ -42,73 +43,105 @@ class DriverInfo extends Component {
     }
 
     onPressSegment(index) {
-        if (this.state.active != index)
-            this.setState({ active: index })
+        if (this.state.active != index) {
+            if (index == 0) {
+                this.props.setGetDriverInfoWaiting()
+                this.setState({ active: 0 })
+                InteractionManager.runAfterInteractions(() => this.props.getDriverInfo({ OptionalParam: { driveId: this.props.initParam.driverId } }))
+            }
+            // if (index == 1) {
+            //     this.props.setGetTruckImageWaiting()
+            //     this.setState({ active: 1 })
+            //     InteractionManager.runAfterInteractions(() => this.props.getTruckImage({
+            //         OptionalParam: { truckId: this.props.initParam.truckId },
+            //         requiredParam: { userId: this.props.userReducer.user.userId, truckNum: this.props.initParam.truckName }
+            //     }))
+            // }
+            // if (index == 2) {
+            //     this.props.setGetTruckInsuranceWaiting()
+            //     this.setState({ active: 2 })
+            //     InteractionManager.runAfterInteractions(() => this.props.getTruckInsurance({ OptionalParam: { truckId: this.props.initParam.truckId, active: 1 } }))
+            // }
+        }
     }
 
     renderDriverInfo() {
-        return (
-            <View style={{}}>
-                <View style={{ paddingHorizontal: 10, paddingVertical: 10, backgroundColor: '#f2f6f9', borderBottomWidth: 0.5, borderColor: '#ccc' }}>
-                    <View style={{ flexDirection: 'row' }}>
-                        <View style={{ width: 40 }}>
-                            <MaterialCommunityIcons name='account' size={20} color='#00cade' />
-                        </View>
-                        <View style={{ flex: 1, flexDirection: 'row' }}>
-                            <Text style={{ color: '#00cade', fontWeight: 'bold' }}>王宝泉</Text>
-                        </View>
-                        <View style={{ flex: 1, flexDirection: 'row' }}>
-                            <Icon color='#00cade' name='md-man' style={{ fontSize: 20, color: '#00cade' }} />
-                            {/* <Icon  name ='md-woman'/> */}
-                        </View>
-                        <View style={{ width: 40 }}>
-                            <FontTag size={26} title='自' color='#12c3eb' fontColor='#fff' />
-                            {/* <FontTag size={30} title='协' color='#73de8a' fontColor='#fff' />
+        const { getDriverInfo } = this.props.driverInfoReducer
+        if (getDriverInfo.isResultStatus == 1) {
+            return (
+                <View style={{ backgroundColor: '#fff', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <ActivityIndicator
+                        animating={getDriverInfo.isResultStatus == 1}
+                        style={{ height: 80 }}
+                        size="large"
+                    />
+                </View>
+            )
+        } else {
+            const { driverInfo } = this.props.driverInfoReducer.data
+            return (
+                <View style={{ flex: 1 }}>
+                    <View style={{ paddingHorizontal: 10, paddingVertical: 10, backgroundColor: '#f2f6f9', borderBottomWidth: 0.5, borderColor: '#ccc' }}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <View style={{ width: 40 }}>
+                                <MaterialCommunityIcons name='account' size={20} color='#00cade' />
+                            </View>
+                            <View style={{ flex: 1, flexDirection: 'row' }}>
+                                <Text style={{ color: '#00cade', fontWeight: 'bold' }}>{driverInfo.drive_name ? driverInfo.drive_name : ''}</Text>
+                            </View>
+                            <View style={{ flex: 1, flexDirection: 'row' }}>
+                                <Icon color='#00cade' name='md-man' style={{ fontSize: 20, color: '#00cade' }} />
+                                {/* <Icon  name ='md-woman'/> */}
+                            </View>
+                            <View style={{ width: 40 }}>
+                                <FontTag size={26} title='自' color='#12c3eb' fontColor='#fff' />
+                                {/* <FontTag size={30} title='协' color='#73de8a' fontColor='#fff' />
                     <FontTag size={30} title='供' color='#efbb7a' fontColor='#fff' />
                     <FontTag size={30} title='包' color='#e08ddd' fontColor='#fff' /> */}
+                            </View>
+                        </View>
+                        <View style={{ flexDirection: 'row', paddingHorizontal: 40 }}>
+                            <View style={{ flex: 2, flexDirection: 'row', alignItems: 'center' }}>
+                                <FontAwesomeIcon name='building-o' size={11} />
+                                <Text style={{ paddingLeft: 5 }}>{driverInfo.company_name ? driverInfo.company_name : ''}</Text>
+                            </View>
+                            <View style={{ flex: 3, flexDirection: 'row', alignItems: 'center' }}>
+                                <FontAwesomeIcon name='mobile-phone' size={16} />
+                                <Text style={{ paddingLeft: 5 }}>{driverInfo.company_name ? driverInfo.company_name : ''}</Text>
+                            </View>
                         </View>
                     </View>
-                    <View style={{ flexDirection: 'row', paddingHorizontal: 40 }}>
-                        <View style={{ flex: 2, flexDirection: 'row', alignItems: 'center' }}>
-                            <FontAwesomeIcon name='building-o' size={11} />
-                            <Text style={{ paddingLeft: 5 }}>安吉物流</Text>
-                        </View>
-                        <View style={{ flex: 3, flexDirection: 'row', alignItems: 'center' }}>
-                            <FontAwesomeIcon name='mobile-phone' size={16} />
-                            <Text style={{ paddingLeft: 5 }}>13887878787</Text>
-                        </View>
+                    <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>关联货车：</Text>辽B12345</Text>
+                    </View>
+                    <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>入职时间：</Text>2017-08-19</Text>
+                    </View>
+                    <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>身份证号：</Text>210210210210210210</Text>
+                    </View>
+                    <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>家庭住址：</Text>始源国际2012</Text>
+                    </View>
+                    <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>紧急联系人电话：</Text>13887878787</Text>
+                    </View>
+                    <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>驾照类型：</Text>A1</Text>
+                    </View>
+                    <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>驾驶证检证日期：</Text>2017-09-10</Text>
+                    </View>
+                    <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>准运证检证日期：</Text>2017-09-10</Text>
+                    </View>
+                    <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
+                        <Text style={{ fontSize: 11, fontWeight: 'bold' }}>备注：</Text>
+                        <Text style={{ fontSize: 11 }}>一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁</Text>
                     </View>
                 </View>
-                <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>关联货车：</Text>辽B12345</Text>
-                </View>
-                <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>入职时间：</Text>2017-08-19</Text>
-                </View>
-                <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>身份证号：</Text>210210210210210210</Text>
-                </View>
-                <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>家庭住址：</Text>始源国际2012</Text>
-                </View>
-                <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>紧急联系人电话：</Text>13887878787</Text>
-                </View>
-                <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>驾照类型：</Text>A1</Text>
-                </View>
-                <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>驾驶证检证日期：</Text>2017-09-10</Text>
-                </View>
-                <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ fontSize: 11 }}><Text style={{ fontWeight: 'bold' }}>准运证检证日期：</Text>2017-09-10</Text>
-                </View>
-                <View style={{ borderBottomWidth: 0.5, borderColor: '#ccc', paddingHorizontal: 10, paddingVertical: 10 }}>
-                    <Text style={{ fontSize: 11, fontWeight: 'bold' }}>备注：</Text>
-                    <Text style={{ fontSize: 11 }}>一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁一只大蚂蚁</Text>
-                </View>
-            </View>
-        )
+            )
+        }
     }
 
     renderDriverPhoto() {
