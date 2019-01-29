be399f62ea6cc25d87ae5f0bd6d8cf5cc10e7b60
rb yu <rbyu@rbdeMacBook-Air.local>
2019-01-29 10:38:27 +0800

    modified FuelFillingApply bug

--- src/android/complatedViews/fuelFillingApply/FuelFillingApplyAction.js
+++ src/android/complatedViews/fuelFillingApply/FuelFillingApplyAction.js
@@ -8,16 +8,21 @@ import { InteractionManager, ToastAndroid } from 'react-native'
 
 export const createFuelFillingApply = (param) => async (dispatch, getState) => {
     try {
+        const { communicationSettingReducer: { data: { base_host } } } = getState()
+        // console.log('base_host', base_host)
         const { loginReducer: { data: { user: { drive_id, uid } } } } = getState()
         const getTruckUrl = `${base_host}/truckFirst?${ObjectToUrl({ driveId: drive_id })}`
+        // console.log('getTruckUrl', getTruckUrl)
+
         const getTruckRes = await httpRequest.get(getTruckUrl)
-        const { communicationSettingReducer: { data: {base_host } } } = getState()
+       
+        
         if (getTruckRes.success) {
             if (getTruckRes.result.length == 0) {
                 dispatch({ type: actionTypes.fuelFillingApplyTypes.CREATE_FuelFilling_FAILED, payload: { data: '未绑定车头！' } })
             } else {
                 const url = `${base_host}/user/${uid}/driveRefuel`
-                console.log('url', url)
+                // console.log('url', url)
                 const res = await httpRequest.post(url, objectExceptNull({
                     driveId: drive_id,
                     truckId: getTruckRes.result[0].id,
@@ -30,7 +35,7 @@ export const createFuelFillingApply = (param) => async (dispatch, getState) => {
                     lat: param.refuelAddress && param.refuelAddress.item ? param.refuelAddress.item.latitude : null,
                     refuelMoney: param.refuelMoney
                 }))
-                console.log('res', res)
+                // console.log('res', res)
                 if (res.success) {
                     ToastAndroid.show('加油申请提交成功！', 10)
                     dispatch({ type: actionTypes.fuelFillingApplyTypes.CREATE_FuelFilling_SUCCESS, payload: { data: {} } })
@@ -47,7 +52,7 @@ export const createFuelFillingApply = (param) => async (dispatch, getState) => {
         }
     } catch (err) {
         ToastAndroid.show(`加油申请提交失败：${err}！`, 10)
-        console.log('err', err)
+        // console.log('err', err)
         dispatch({ type: actionTypes.fuelFillingApplyTypes.CREATE_FuelFilling_ERROR, payload: { data: err } })
     }
 }
