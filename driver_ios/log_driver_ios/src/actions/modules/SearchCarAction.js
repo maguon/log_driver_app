1889e1086ace69097ac102d1308a482a6779f3be
suchenglong <chenglong.su@myxxjs.com>
2019-07-29 10:09:03 +0800

    新建：Search Car

---
+++ driver_ios/log_driver_ios/src/actions/modules/SearchCarAction.js
@@ -0,0 +1,39 @@
+import httpRequest from '../../util/HttpRequest'
+import * as actionTypes from '../../actionTypes/index'
+import { ObjectToUrl } from '../../util/ObjectToUrl'
+import { getFormValues } from 'redux-form'
+
+
+const pageSize = 10
+
+export const getCarList = req => async (dispatch, getState) => {
+    try {
+        const { communicationSettingReducer: { data: { base_host} } } = getState()
+        const searchFormValues = getFormValues('searchCarForm')(getState())
+        const url = `${base_host}/carList?${ObjectToUrl({
+            vinCode: searchFormValues ? searchFormValues.vin : null,
+            start: 0,
+            carStatusArr: '1,2',
+            size: pageSize,
+            ...req
+        })}`
+        // console.log('url',url)
+        const res = await httpRequest.get(url)
+        if (res.success) {
+            dispatch({ type: actionTypes.searchCarTypes.search_carListForSelect_success, payload: { carList: res.result } })
+        } else {
+            dispatch({ type: actionTypes.searchCarTypes.search_carListForSelect_failed, payload: { failedMsg: res.msg } })
+        }
+    } catch (err) {
+        dispatch({ type: actionTypes.searchCarTypes.search_carListForSelect_error, payload: { errorMsg: err } })
+    }
+}
+
+export const getCarListWaiting = () => (dispatch) => {
+    dispatch({ type: actionTypes.searchCarTypes.search_carListForSelect_waiting, payload: {} })
+}
+
+
+export const cleanCarList = () => (dispatch) => {
+    dispatch({ type: actionTypes.searchCarTypes.clean_search_carListForSelect, payload: {} })
+}
---
+++ driver_ios/log_driver_ios/src/reducers/modules/SearchCarReducer.js
@@ -0,0 +1,67 @@
+import { handleActions } from 'redux-actions'
+import * as actionTypes from '../../actionTypes/index'
+
+const initialState = {
+    data: {
+        carList: []
+    },
+    getCarList: {
+        errorMsg: '',
+        failedMsg: '',
+        isResultStatus: 0
+    }
+}
+//isResultStatus(执行结果状态):[0(未执行),1(等待)，2(成功)，3(错误)，4(执行失败),5(服务器未处理错误)]
+export default handleActions({
+    [actionTypes.searchCarTypes.search_carListForSelect_success]: (state, action) => {
+        const { payload: { carList } } = action
+        return {
+            ...state,
+            data: {
+                carList
+            },
+            getCarList: {
+                ...initialState.getCarList,
+                isResultStatus: 2
+            }
+        }
+    },
+    [actionTypes.searchCarTypes.search_carListForSelect_failed]: (state, action) => {
+        const { payload: { failedMsg } } = action
+        return {
+            ...state,
+            getCarList: {
+                ...initialState.getCarList,
+                isResultStatus: 4,
+                failedMsg
+            }
+        }
+    },
+    [actionTypes.searchCarTypes.search_carListForSelect_waiting]: (state, action) => {
+        return {
+            ...state,
+            getCarList: {
+                ...initialState.getCarList,
+                isResultStatus: 1
+            }
+        }
+    },
+    [actionTypes.searchCarTypes.search_carListForSelect_error]: (state, action) => {
+        const { payload: { errorMsg } } = action
+        return {
+            ...state,
+            getCarList: {
+                ...initialState.getCarList,
+                isResultStatus: 3,
+                errorMsg
+            }
+        }
+    },
+
+    [actionTypes.searchCarTypes.clean_search_carListForSelect]: (state, action) => {
+        return {
+            ...initialState
+        }
+    }
+}, initialState)
+
