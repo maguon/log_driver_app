08c39a35dd4c526fb49230a875828a25a4f1bd01
suchenglong01 <15840668526@163.com>
2020-12-30 11:15:04 +0800

    新建：markdown  02

--- src/android/complatedViews/notification/Notification.js
+++ src/android/complatedViews/notification/Notification.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Text, StyleSheet, View, ScrollView} from 'react-native'
-import {Container, Form, Content, Button, ListItem} from 'native-base'
-import globalStyles from '../../GlobalStyles'
+import {Container, Form, Content, Button, ListItem, Spinner} from 'native-base'
+import globalStyles, {styleColor} from '../../GlobalStyles'
 import {connect} from 'react-redux'
 import Markdown from 'react-native-markdown-display';
 import moment from 'moment'
@@ -60,41 +60,51 @@ this is an example for adding picture:
 
 
 const Notification = props => {
-    const {id, notificationReducer: {data: {notification}}} = props
+    const {id, notificationReducer: {data: {notification},getNotification}} = props
 
-    return (
-        <Container style={globalStyles.container}>
+    if (getNotification.isResultStatus == 1) {
+        return (
+            <Container>
+                <Spinner color={styleColor} />
+            </Container>
+        )
+    }
+    else {
+        return (
+            <Container style={globalStyles.container}>
 
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={{height: '100%'}}
-            >
-                <View style={{padding: 8, margin: 8}}>
-                    <View style={styles.title}>
-                        <Text style={{
-                            fontSize: 20,
-                            color: '#777'
-                        }}>{notification.title ? `${notification.title}` : ''}</Text>
+                <ScrollView
+                    contentInsetAdjustmentBehavior="automatic"
+                    style={{height: '100%'}}
+                >
+                    <View style={{padding: 8, margin: 8}}>
+                        <View style={styles.title}>
+                            <Text style={{
+                                fontSize: 20,
+                                color: '#777'
+                            }}>{notification.title ? `${notification.title}` : ''}</Text>
+                        </View>
+                        <Markdown>
+                            {notification.content}
+                        </Markdown>
+                        <View style={styles.fotter}>
+                            <Text
+                                style={[globalStyles.midText]}>作者：{notification.real_name ? `${notification.real_name}` : ''}</Text>
+                            <Text
+                                style={[globalStyles.midText]}>发布时间：{notification.created_on ? `${moment(notification.created_on).format('YYYY-MM-DD HH:mm:ss')}` : ''}</Text>
+                        </View>
                     </View>
-                    <Markdown>
-                        {notification.content}
-                    </Markdown>
-                    <View style={styles.fotter}>
-                        <Text
-                            style={[globalStyles.midText]}>作者：{notification.real_name ? `${notification.real_name}` : ''}</Text>
-                        <Text
-                            style={[globalStyles.midText]}>发布时间：{notification.created_on ? `${moment(notification.created_on).format('YYYY-MM-DD HH:mm:ss')}` : ''}</Text>
-                    </View>
-                </View>
-            </ScrollView>
+                </ScrollView>
 
-            {/*<View style={{ padding: 7.5, flexDirection: 'row', justifyContent: 'space-between' }}>*/}
-            {/*    <Text style={[globalStyles.midText]}>{notification.real_name ? `${notification.real_name}` : ''}</Text>*/}
-            {/*    <Text style={[globalStyles.midText]}>{notification.title ? `${notification.title}` : ''}</Text>*/}
-            {/*    <Text style={[globalStyles.midText]}>{notification.content ? `${notification.content}` : ''}</Text>*/}
-            {/*</View>*/}
-        </Container>
-    )
+                {/*<View style={{ padding: 7.5, flexDirection: 'row', justifyContent: 'space-between' }}>*/}
+                {/*    <Text style={[globalStyles.midText]}>{notification.real_name ? `${notification.real_name}` : ''}</Text>*/}
+                {/*    <Text style={[globalStyles.midText]}>{notification.title ? `${notification.title}` : ''}</Text>*/}
+                {/*    <Text style={[globalStyles.midText]}>{notification.content ? `${notification.content}` : ''}</Text>*/}
+                {/*</View>*/}
+            </Container>
+        )
+
+    }
 }
 
 const mapStateToProps = (state) => {
